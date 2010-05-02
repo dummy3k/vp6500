291866a00c19ee2c02081bfe45189f89cad8f07b
dummy3k <l4711@gmx.net>
2010-05-02 15:07:23 +0200

    made converting a bit faster

--- convert_rgb565.py
+++ convert_rgb565.py
@@ -4,53 +4,29 @@ from StringIO import StringIO
 from leopytools.helpers import ipython
 from optfunc import optfunc
 
-def convert_rgb565(filename):
+
+def convert_rgb565(filename, showImage=False):
     infile = open(filename, 'rb')
-    __convert_rgb565__(infile)
+    __convert_rgb565__(infile, showImage)
     infile.close()
 
-def __convert_rgb565__(buffer):
+def __convert_rgb565__(buffer, showImage=False):
     img = Image.new("RGB", (240, 220))
-    definitely = [('green', (156, 11)),
-                  ('blue',  (47, 46)),
-                  ('red',  (25, 165))]
-
+    putpixel = img.putpixel
     unique = []
     for y in range(220):
         for x in range(240):
-            definitely_color = None
-            for color, coords in definitely:
-                if (x, y) == coords:
-                    definitely_color = color
-                    break
-                    
             raw = buffer.read(2)
             if not raw:
                 print "Unexpected no data"
                 return
 
             raw_ushort = struct.unpack('H', raw)[0]
+            #~ raw_touple = ord(raw[0]), ord(raw[1])
 
-            #~ r = raw >> (6+5)
-            #~ if x == 10 and y > 25 and y < 35:
-                #~ print "%04x (%s)" % (raw, r)
-            #~ continue
-            
-            raw_touple = ord(raw[0]), ord(raw[1])
-            if definitely_color:
-                print "%s\traw_ushort: %04x" % (definitely_color, raw_ushort)
-                print "%s\traw_ushort: % 20s" % (definitely_color, bin(raw_ushort))
-                print "%s\traw_touple: %02x, %02x" % (definitely_color, raw_touple[0], raw_touple[1])
-
-            #~ r = raw_touple[0] >> 3
-            #~ g = raw_touple[0] & 0b111
-            #~ g = g << 3
-            #~ g += (raw_touple[1] & 0b111000) >> 3
-            #~ b = raw_touple[1] & 0b11111
             r = raw_ushort >> (6 + 5)
             g = raw_ushort >> 5 & 0b111111
             b = raw_ushort & 0b11111
-            
 
             r *= 8
             g *= 4
@@ -59,10 +35,6 @@ def __convert_rgb565__(buffer):
                 print "bad values (%s, %s, %s)" % (r, g, b)
                 return
 
-            #~ tmp = r
-            #~ r = b
-            #~ b = tmp
-            
             color_tuple = (r, g, b)
 
             #~ r = 0
@@ -71,23 +43,23 @@ def __convert_rgb565__(buffer):
             color = r
             color += g << 8
             color += b << 16
-            log_msg = "<(%02x%02x) -> (%s, %s, %s) %X>" % (raw_touple[0], raw_touple[1], r, g, b, color)
-            if not log_msg in unique:
-                unique.append(log_msg)
+            #~ log_msg = "<(%02x%02x) -> (%s, %s, %s) %X>" % (raw_touple[0], raw_touple[1], r, g, b, color)
+            #~ if not log_msg in unique:
+                #~ unique.append(log_msg)
+#~ 
+            #~ if definitely_color:
+                #~ print definitely_color + "\t" + log_msg
 
-            if definitely_color:
-                print definitely_color + "\t" + log_msg
-
-            img.putpixel( (x, y), color)
+            putpixel( (x, y), color)
 
     for raw_touple in unique:
         #~ print raw_touple
         pass
 
-    print "Hello"
-    img.show()
+    if showImage:
+        img.show()
 
-def download(host, port=31337):
+def download(host, port=31337, showImage=False):
     print "%s:%s" % (host, port)
 
     # 2*240*220 = 105600
@@ -105,8 +77,23 @@ def download(host, port=31337):
 
     data = data[:WHOLE_SIZE]
     print "Ok: %s" % len(data)
-    __convert_rgb565__(StringIO(data))
+    __convert_rgb565__(StringIO(data), showImage)
 
-if __name__ == '__main__':
+
+def main():
     #~ optfunc.run(convert_rgb565)
     optfunc.main([convert_rgb565,download])
+
+if __name__ == '__main__':
+    main()
+
+    #~ import cProfile
+    #~ profile_filename = __file__ + '.profile'
+    #~ cProfile.run('main()', profile_filename)
+#~ 
+    #~ import pstats
+    #~ p = pstats.Stats(profile_filename)
+    #~ p.strip_dirs().sort_stats('cumulative').print_stats(10)
+    #~ p.strip_dirs().sort_stats('time').print_stats(10)
+    #~ p.strip_dirs().sort_stats('time', 'cum').print_stats(10)
+    #~ p.strip_dirs().sort_stats(-1).print_stats(10)
--- fbdaemon.sh
+++ fbdaemon.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# micro_inetd 31337 fbdaemon.sh
+
 # 2*240*220 = 105600
 dd if=/dev/fb bs=105600 count=1
